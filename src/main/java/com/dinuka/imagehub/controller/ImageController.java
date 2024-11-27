69b0afe7a032a2d90b13710f4ffff42897ba8dca
dinuka00 <tdammage10@gmail.com>
2024-11-27 12:26:45 +0530

    image controller updated

--- src/main/java/com/dinuka/imagehub/controller/ImageController.java
+++ src/main/java/com/dinuka/imagehub/controller/ImageController.java
@@ -4,6 +4,8 @@ import com.dinuka.imagehub.dto.ImageDTO;
 import com.dinuka.imagehub.entity.Image;
 import com.dinuka.imagehub.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,30 +18,51 @@ public class ImageController {
     private ImageService imageService;
 
     @PostMapping("/images")
-    public Object uploadImage(@RequestParam("file") MultipartFile file,
-                              @RequestParam(value = "categoryId", required = false) Integer categoryId) {
-        return imageService.save(file, categoryId);
+    public ResponseEntity<Object> uploadImage(@RequestParam("file") MultipartFile file,
+                                             @RequestParam(value = "categoryId", required = false)
+                                             Integer categoryId) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(imageService.save(file, categoryId));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @PutMapping("/images/{id}")
-    public Object updateImage(@RequestBody ImageDTO imageDTO, @PathVariable Long id) {
+    public ResponseEntity<Object> updateImage(@RequestBody ImageDTO imageDTO, @PathVariable Long id) {
 
-        return imageService.update(imageDTO,id);
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(imageService.update(imageDTO,id));
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @GetMapping("/image/{id}")
-    public Image getImage(@PathVariable Long id) {
-        return imageService.findById(id);
+    public ResponseEntity<Image> getImage(@PathVariable Long id) {
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(imageService.findById(id));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @GetMapping("/images")
-    public List<Image> getImages() {
-        return imageService.findAll();
+    public ResponseEntity<List<Image>> getImages() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(imageService.findAll());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DeleteMapping("/image/{id}")
-    public String deleteImage(@PathVariable Long id) {
-        return imageService.deleteById(id);
+    public ResponseEntity<String> deleteImage(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(imageService.deleteById(id));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-
 }
